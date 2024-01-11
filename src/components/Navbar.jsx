3ee1d103bfdc687a3c52f7570ba6658c3aa67fd7
Adnanyousafzai21 <www.adnanrafiq@gmail.com>
2024-01-11 16:25:47 +0500

    last commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 import Profileimg from './Profileimg'
 const Navbar = () => {
     return (
-        <div className='w-full bg-white text-sky-500  sticky top-0 z-30'>
-            <div className='max-w-[750px]   mx-auto '>
+        <div className='w-full bg-white text-sky-500 px-3 md:px-0 sticky top-0 z-30'>
+            <div className='max-w-[750px]  py-3 mx-auto '>
                 <div className='flex justify-between'>
                     <div className="">
                         <Profileimg />
