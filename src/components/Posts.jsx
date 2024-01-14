43079c28faaaae9dff2e3bd798bbb84450d04a14
Adnanyousafzai21 <www.adnanrafiq@gmail.com>
2024-01-14 19:46:16 +0500

    commit

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -26,7 +26,7 @@ const Posts = ({ pupdate }) => {
     return (
         <div className='w-full  bg-customwhite rounded-md my-2 py-2 px-5  '>
             {
-                data && data?.reverse().map((data) => {
+                data && data?.map((data) => {
                     return <div className='flex flex-col gap-2 my-3' key={data._id}>
                         <div className="flex justify-between"><ProfileTitle avater={data.user?.avater} fullname={data?.user?.fullname} time={data?.createdAt} />
                          <PostDeletUpdate postId={data._id}  /></div>
