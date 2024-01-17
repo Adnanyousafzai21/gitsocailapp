3ff6d86a1ae136c6f652a46b0cdf696d149a6a60
Adnanyousafzai21 <www.adnanrafiq@gmail.com>
2024-01-17 16:40:54 +0500

    dsfsdfd

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,12 +13,12 @@ const Main = ({ children }) => {
 
   return (
     <div className=' max-w-[900px]  flex flex-col bg-slate-200 py-2 gap-5 items-center md:items-start  md:flex-row m-auto  justify-center'>
-      <div className={`md:w-[25%]  border w-[95%]
-      ${isDesktopOrTablet && location.pathname === "/profile" ? "hidden" : ""} sticky top-14 md:block hidden`}>
+      <div className={`md:w-[25%]  border w-[95%] hidden
+      ${isDesktopOrTablet && location.pathname === "/profile" ? "hidden" : ""} sticky top-14  hidden`}>
         <Stories />
         </div>
       <div className='md:w-[40%]  w-[95%] border '>{children}</div>
-      <div className='md:w-[25%] w-[95%]  hidden lg:block sticky top-14'>
+      <div className='md:w-[25%] w-[95%]  hidden  sticky top-14'>
         <Features />
       </div>
     </div>
